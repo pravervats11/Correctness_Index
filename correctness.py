38c120261ce6310156d378d53ceefa71784d9176
Praver Vats <pv335@snu.edu.in>
2017-08-24 20:05:53 +0530

    Update correctness.py

--- correctness.py
+++ correctness.py
@@ -5,3 +5,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVR
 import datetime
+
+df_train = pd.read_csv('file_name', delimiter="|")
+df_test = pd.read_csv('file_name', delimiter="|")
