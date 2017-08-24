bed7d2bf233aae9cca0e2b0e62e0ffa3cfaa77c5
Praver Vats <pv335@snu.edu.in>
2017-08-24 20:04:41 +0530

    Create correctness.py

---
+++ correctness.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVR
+import datetime
